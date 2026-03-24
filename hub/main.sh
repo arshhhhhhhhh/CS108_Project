ae90ee7061771b239bf41ddb18b9fa74d86fd310
arshhhhhhhhh <poiuytrewqheyhow@gmail.com>
2026-03-24 19:16:39 +0000

    Added Hash Pass function and fixed some errors

--- hub/main.sh
+++ hub/main.sh
@@ -2,8 +2,8 @@
 #take username and password
 
 login_user () {
-    read -p "Enter user $1's username." usr;
-    read -sp "Enter user $1's password." psd;
+    read -p "Enter user $1's username: " usr;
+    read -sp "Enter user $1's password: " psd;
     tsvd=$(cut -f1,2 -d\t users.tsv);
     stg=0;
     us_f=0;
@@ -21,8 +21,8 @@ login_user () {
             stg=0
         fi
     done
-    if [ $us_f -eq 0 ]; do
-        read -p "Given username does not exist. Do you want to create new user? (y/n)" ch
+    if [ $us_f -eq 0 ]; then
+        read -p "Given username does not exist. Do you want to create new user? (y/n) " ch
         if [ $ch == "y" ]; then
             register_user $1
         else
@@ -38,9 +38,9 @@ login_user () {
 }
 
 register_user () {
-    read -p "Enter new username." usr_n;
-    read -sp "Enter new password." psd_n;
-    uss=$(cut -f1 -d'\t' users.tsv);
+    read -p "Enter new username: " usr_n;
+    read -sp "Enter new password: " psd_n;
+    uss=$(cut -f1 -d\t users.tsv);
     us_f=0;
     for i in uss; do
         if [ $usr_n == $i ]; then
@@ -48,8 +48,8 @@ register_user () {
             break;
         fi
     done
-    if [ $us_f -eq 0 ]; do
-        echo "$(usr_n)\t$(hash_pass psd_n)" >> users.tsv
+    if [ $us_f -eq 0 ]; then
+        echo -e "$usr_n\t$(hash_pass psd_n)" >> users.tsv
     else
         echo "Given username already exists. Please use a different username."
         register_user $1
@@ -58,6 +58,7 @@ register_user () {
 
 hash_pass () {
     #Hashing Password
+    echo $1 | sha256sum
 }
 
 login_user 1
